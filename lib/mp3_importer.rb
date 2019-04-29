af7b0805db235bd007d402fa5bd663f69f11f2e8
James Declerus <james.declerus@esih.edu>
2019-04-29 15:41:13 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,15 +1,13 @@
 class MP3Importer
 
-
-
-
-
-
-
-
-
-
-  def import(list_of_filenames)
-    list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
-  end
+  attr_reader :path, :files
+  
+     def initialize(path)
+      @path = path
+      @files = Dir.entries(path).grep(/.*\.mp3/)
+    end
+  
+     def import
+      @files.each {|file| Song.new_by_filename(file)}
+    end
 end
