b9900ba5e0b83b39299f09786bea80c7313cce3a
James Declerus <james.declerus@esih.edu>
2019-04-29 15:43:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,12 @@
-class Song
-  attr_accessor :title ,:artist
+attr_accessor :name, :artist
 
-  def self.new_by_filename(filename)
-    song = self.new
-    song.title = filename.split(" - ")[1]
-    song
+   def initialize(name)
+    @name = name
   end
-  def artist_name=(name)
-      if (self.artist.nil)
-        self.artist = Artist.new(name)
-      else
-        self.artist.name = name
-      end
-    end
-end
+
+   def self.new_by_filename(filename)
+    artist_name, song_name, extra = filename.split(" - ")
+    song = self.new(song_name)
+    artist = Artist.find_or_create_by_name(artist_name)
+    artist.add_song(song)
+  end
