513a88f3edcd6f9bdc707d8c999bc8f0e5be86a8
James Declerus <james.declerus@esih.edu>
2019-04-29 15:35:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,7 @@ class Song
     song
   end
   def artist_name=(name)
-      if (self.artist.nil ?)
+      if (self.artist.nil)
         self.artist = Artist.new(name)
       else
         self.artist.name = name
